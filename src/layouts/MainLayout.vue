49d70f478f28ed57028674f62ccc6ef7117b7db6
sfestacatenate <simonefesta1@gmail.com>
2024-04-13 18:40:35 +0200

    First Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header>
+          Navigation
+        </q-item-label>
+
+        <q-item clickable @click="$router.push('/')" v-close-popup>
+          <q-item-section>
+            <q-item-label>Compare</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable @click="$router.push('/comparations')" v-close-popup>
+          <q-item-section>
+            <q-item-label>List</q-item-label>
+          </q-item-section>
+        </q-item>
+
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineOptions({
+  name: 'MainLayout'
+});
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
